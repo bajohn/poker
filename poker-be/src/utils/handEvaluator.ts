61c90a32d02cc0b4f7132f27d65bca9406026c44
Brendan John <bjohn454@gmail.com>
2021-08-14 14:49:40 -0400

    identify hand  match types

--- poker-be/src/utils/handEvaluator.ts
+++ poker-be/src/utils/handEvaluator.ts
@@ -1,5 +1,7 @@
 import { iCard } from "../../../shared/sharedtypes"
 
+const TEN_K = 1e4;
+const ONE_M = 1e6;
 // Return an array of all possible permutations 
 // of the given cards array 
 export const permuteCards = (cards: iCard[], permSize: number) => {
@@ -20,17 +22,24 @@ export const permuteCards = (cards: iCard[], permSize: number) => {
 export const rankSort = (cards: iCard[]) => cards.sort((a, b) => b.value - a.value);
 
 // TODO finish this!
+// return
+// array of matches? 
+// [{val, count}, {val, count},...]
 export const getMatches = (cards: iCard[]) => {
+
     const sortedCards = rankSort(cards);
     let matchCtr = 0;
     const kickers = [];
-    const matches: { [key: string]: number } = {};
-    for (let i = 0; i < sortedCards.length - 1; i++) {
-        if (sortedCards[i].value === sortedCards[i + 1].value) {
+    const matches: matches[] = [];
+    for (let i = 1; i < sortedCards.length; i++) {
+        if (sortedCards[i - 1].value === sortedCards[i].value) {
             matchCtr += 1;
         } else {
             if (matchCtr > 0) {
-                matches[sortedCards[i].value + ''] = matchCtr + 1;
+                matches.push({
+                    value: sortedCards[i].value,
+                    count: matchCtr
+                });
                 matchCtr = 0;
             } else {
                 kickers.push(sortedCards[i].value);
@@ -38,6 +47,35 @@ export const getMatches = (cards: iCard[]) => {
         }
     }
     return { matches, kickers };
+}
+
+export const classifyMatches = (cards: iCard[]) => {
+    if (cards.length !== 5) {
+        throw Error('Incorrect sized card array sent to classifyMatches()!')
+    }
+    const { matches, kickers } = getMatches(cards);
+    if (matches.length === 2) {
+        if (matches[0].count === 3 || matches[1].count === 3) {
+            // full house
+        } else {
+            // two pair 
+        }
+
+
+    } else if (matches.length === 1) {
+        if (matches[0].count === 3) {
+            // three of kind
+        } else {
+            // pair
+        }
+    } else {
+        return -1;
+    }
+}
+
+interface matches {
+    value: number
+    count: number
 }
 
 
